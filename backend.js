66493e01b76ce988dd44cfff7c8d3dbb67ff72b7
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-07-02 11:53:04 -0400

    Added .env file to store env variables. Established the connection between nodejs app and mongoDB server

--- backend.js
+++ backend.js
@@ -1,6 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+dotenv.config();
+const uri = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@instagram-clone.gxdemf6.mongodb.net/?retryWrites=true&w=majority`;
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected successfully to mongodb server");
+  } catch (err) {
+    console.error(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.connection.close();
+  }
+}
+connect();
 import * as url from 'url';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
