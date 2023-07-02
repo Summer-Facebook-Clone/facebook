1204fe890e30c6a7225e1b2000595244fac36d5f
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-07-02 11:57:38 -0400

    Made the code more organized

--- backend.js
+++ backend.js
@@ -2,36 +2,35 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { fileURLToPath } from 'url';
+
 dotenv.config();
+
 const uri = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@instagram-clone.gxdemf6.mongodb.net/?retryWrites=true&w=majority`;
 
 async function connect() {
   try {
     await mongoose.connect(uri);
-    console.log("Connected successfully to mongodb server");
+    console.log("Connected successfully to MongoDB server");
   } catch (err) {
     console.error(err);
   }
-  finally {
-    // Ensures that the client will close when you finish/error
-    await mongoose.connection.close();
-  }
 }
+
 connect();
-import * as url from 'url';
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, () => {
-    console.log('server started');
-    }
-);
+  console.log('Server started');
+});
