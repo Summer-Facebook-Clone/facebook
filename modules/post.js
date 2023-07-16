16abbd91eb80cb0789593da8baaec8629341ddbb
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-07-16 12:12:05 -0400

    Created a post schema for making new posts and made a one to many relationshipe betweem User and Post.

--- modules/post.js
+++ modules/post.js
@@ -1,4 +1,17 @@
-// import mongoose from "mongoose";
-// const { Schema } = mongoose.Schema;
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
 
-// const postSchema = new Schema({
+//We create a schema for the post. This tells us that whenever we create a new post, it must have a particular structure.
+const postSchema = new Schema(
+  {
+    caption: { type: String },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+//Post gets translated to Posts collection automatically in the database that's why we use Post instead of Posts
+const Post = mongoose.model("Post", postSchema);
+
+//We export the Post model so that we can use it in other files
+export { Post };
--- modules/user.js
+++ modules/user.js
@@ -1,16 +1,32 @@
 import mongoose from "mongoose";
-const Schema  = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 // We create a schema for the user. This tells us that whenever we create a new user, it must have a particular structure.
-const userSchema = new Schema({
-    username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
     password: { type: String, required: true, trim: true, minlength: 3 },
-    email: { type: String, required: true, unique: true, trim: true, minlength: 3 },
-    full_name: { type: String, required: true, trim: true, minlength: 3 }
-}, {timestamps: true});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    full_name: { type: String, required: true, trim: true, minlength: 3 },
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  },
+  { timestamps: true }
+);
 
 // User gets translated to Users collection automatically in the database that's why we use User instead of Users
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // We export the User model so that we can use it in other files
 export { User };
