67a6f1eb9fc1ac37ea9e9c738ca88bcce7b1b60a
Seyed Amirreza Mojtahedi <seyed.mojtahedi@dawsoncollege.qc.ca>
2023-07-18 16:01:27 -0400

    Moved CRUD functions to the good file

--- backend.js
+++ backend.js
@@ -10,6 +10,7 @@ import { Post } from "./modules/post.js";
 import { authenticate } from "./config/server.js";
 import { user_finder } from "./config/passport-config.js";
 import { check_authentication, not_authenticated, password_hasher } from "./controllers/auth-controller.js";
+import { user_creator, post_creator_from_instagram, instagram_media_fetcher } from "./controllers/db-crud-controller.js";
 
 let url_token=null;
 // Number of salt rounds for bcrypt hashing
@@ -27,10 +28,10 @@ app.get("/", check_authentication, (req, res) => {
 // Profile route
 app.get("/home", check_authentication, (req, res) => {
   res.sendFile(__dirname + "/profile.html");
-  // instagram_media_fetcher(
-  //   req.user,
-  //   "https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=IGQVJXVTFpMTc2VlhKekVydDN1dmwzZAHVLZAkNsQTNtazAxV1NMRE45RGZAZAeVBFVC1wdEx6ZA0RLQ1Fvd21KTnQySmZAXX1JSOFhiQWlaOUtDX3RKcVJxMmZAoVTBuV3VFbEtHenhkRWRaM1lQWm9FTlZArdAZDZD"
-  // );
+  instagram_media_fetcher(
+    req.user,
+    "https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token=IGQVJXVTFpMTc2VlhKekVydDN1dmwzZAHVLZAkNsQTNtazAxV1NMRE45RGZAZAeVBFVC1wdEx6ZA0RLQ1Fvd21KTnQySmZAXX1JSOFhiQWlaOUtDX3RKcVJxMmZAoVTBuV3VFbEtHenhkRWRaM1lQWm9FTlZArdAZDZD"
+  );
 });
 
 // Sign up route
@@ -132,70 +133,6 @@ app.post("/reset-password/:id/:token",async (req,res)=>{
     res.send("Invalid token");
   }
 });
-
-/**
- * Adds a new user to the database.
- * @param {string} email - The email of the user.
- * @param {string} full_name - The full name of the user.
- * @param {string} username - The username of the user.
- * @param {string} password - The password of the user.
- * @returns {void}
- */
-function user_creator(email, full_name, username, password) {
-  const user = new User({
-    username: username,
-    password: password,
-    email: email,
-    full_name: full_name,
-  });
-  user.save().catch((err) => console.error(err));
-}
-
-/**
- * Creates a new post for the current user based on the image url that is fetched using Instagram basic display API.
- * @param {User} user - The user that is currently logged in.
- * @param {string} image_url - The URL of the image to be posted.
- * @param {string} caption - The caption of the post.
- */
-async function post_creator_from_instagram(user, image_url, caption) {
-  const post = await Post.create({
-    caption: caption,
-    image_url: image_url,
-    user: user._id,
-  });
-  user.posts.push(post._id);
-  await user.save();
-}
-
-/**
- * Fetches user's media from the Instagram API and saves it to the current user's document in the database.
- * @param {string} url - The URL of the Instagram API endpoint to fetch the media from.
- * @param {User} current_user - The user that is currently logged in.
- * @returns {void}
- */
-async function instagram_media_fetcher(current_user, url) {
-  try {
-    let response = await axios.get(url);
-
-    while (true) {
-      for (let i = 0; i < response.data.data.length; i++) {
-        await post_creator_from_instagram(
-          current_user,
-          response.data.data[i].media_url,
-          response.data.data[i].caption
-        );
-      }
-
-      if (response.data.paging.next) {
-        response = await axios.get(response.data.paging.next);
-      } else {
-        break;
-      }
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 /* Commented functions that can be useful later in the program */
 
